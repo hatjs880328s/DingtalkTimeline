26f50a6644bb00520c5a0d171f28851b478d6452
Noah_Shan <shanwzh@inspur.com>
2019-08-15 15:49:51 +0800

    版本:
    还差  链条深度属性赋值方法
    
    更新内容：
    
    Author: Noah_shan

---
+++ TimeLine/TimeLine/Timeline/TimelineProgress.swift
@@ -0,0 +1,209 @@
+//
+//  *******************************************
+//  
+//  TimelineProgress.swift
+//  TimeLine
+//
+//  Created by Noah_Shan on 2019/8/15.
+//  Copyright © 2018 Inpur. All rights reserved.
+//  
+//  *******************************************
+//
+
+
+import UIKit
+
+/*
+ Feature:
+
+ 1.将每一个事件处理为一个对象，这个对象属性有： 开始时间 结束时间 自己层级 自己所在链表中item个数 debugnickname
+ 2.从第一级别开始查找：与“第一个”没有时间交集的都作为第一集别的事件
+
+ 3.从剩下的里面查找，与第一个没有时间交集的作为第二级别的事件
+ 4.遍历每一个第二级别的事件，找出与此事件有交集的第一级别事件，将有交集的事件作为当前事件的父节点存储起来，
+    同时设置这些父节点的层级数为当前层级[这一步为递归操作]
+
+ 5.循环执行3、4步骤直到没有剩余的事件
+
+ 6.由于是引用类型，所有事件处理完毕，按照类中的层级、层级个数获取事件生成的view的frame
+ */
+
+class TimelineProgress: NSObject {
+
+    /// 外部数据源数组
+    var allItems: [OutLinkItemModel] = []
+
+    /// 内部数据源数组
+    var progressItems: [LinkItem]  = []
+
+    /// 按层级拆分后的二维数组
+    var lvGroups: [[LinkItem]] = []
+
+    override init() {
+        super.init()
+        initData()
+    }
+
+    /// 测试做的外部数据源[开始时间排序处理得数据]
+    func initData() {
+        /// 2019 - 8 - 15  14：00 - 15：00
+        let item1 = OutLinkItemModel()
+        item1.startTime = Date(timeIntervalSince1970: 1565848800)
+        item1.startTime = Date(timeIntervalSince1970: 1565852400)
+        self.allItems.append(item1)
+        /// 2019 - 8 - 15  14：30 - 19：30
+        let item2 = OutLinkItemModel()
+        item2.startTime = Date(timeIntervalSince1970: 1565850600)
+        item2.startTime = Date(timeIntervalSince1970: 1565868600)
+        self.allItems.append(item2)
+        /// 2019 - 8 - 15  14：45 - 15：30
+        let item3 = OutLinkItemModel()
+        item3.startTime = Date(timeIntervalSince1970: 1565851500)
+        item3.startTime = Date(timeIntervalSince1970: 1565854205)
+        self.allItems.append(item3)
+        /// 2019 - 8 - 15  15：30 - 16：00
+        let item4 = OutLinkItemModel()
+        item4.startTime = Date(timeIntervalSince1970: 1565854205)
+        item4.startTime = Date(timeIntervalSince1970: 1565856000)
+        self.allItems.append(item4)
+        /// 2019 - 8 - 15  17：00 - 18：00
+        let item5 = OutLinkItemModel()
+        item5.startTime = Date(timeIntervalSince1970: 1565859600)
+        item5.startTime = Date(timeIntervalSince1970: 1565863200)
+        self.allItems.append(item5)
+        /// 2019 - 8 - 15  17：30 - 18：00
+        let item6 = OutLinkItemModel()
+        item6.startTime = Date(timeIntervalSince1970: 1565861400)
+        item6.startTime = Date(timeIntervalSince1970: 1565863200)
+        self.allItems.append(item6)
+        /// 2019 - 8 - 15  20：00 - 21：00
+        let item7 = OutLinkItemModel()
+        item7.startTime = Date(timeIntervalSince1970: 1565870400)
+        item7.startTime = Date(timeIntervalSince1970: 1565874000)
+        self.allItems.append(item7)
+    }
+
+    /// 将外部数据数组转为内部数据数组
+    func progressOutData2InnerData() {
+        for eachItem in self.allItems {
+            let realItem = LinkItem(model: eachItem)
+            self.progressItems.append(realItem)
+        }
+    }
+
+    /// 从某一个数组中获取某一级别的数据，并做引用存储
+    private func getEachLvlItems(lvl: Int, arrs: [LinkItem]) {
+        if arrs.count == 0 { return }
+        var endTime = Date()
+        var result = [LinkItem]()
+        for idx in 0 ..< arrs.count {
+            if idx == 0 {
+                result.append(arrs[idx])
+                endTime = arrs[idx].endTime
+                arrs[idx].lvl = lvl
+                continue
+            }
+            if arrs[idx].startTime >= endTime {
+                result.append(arrs[idx])
+                endTime = arrs[idx].endTime
+                arrs[idx].lvl = lvl
+            }
+        }
+        self.lvGroups.append(result)
+    }
+
+    /// 遍历某一个层级的所有item,找到所有它的父节点，添加到自己属性中
+    private func loopProgressEachLvItems(arrs: [LinkItem]) {
+        guard let firstItem = arrs.first else { return }
+        let currentIvl: Int = firstItem.lvl
+        if currentIvl == 0 { return }
+        let fatherLvl: Int = currentIvl - 1
+        /// 所有上级事件
+        let fatherGroup = self.lvGroups[fatherLvl]
+        for eachItem in arrs {
+            /// 从所有上级事件中寻找与自己有交集的item
+            for eachFatherItem in fatherGroup {
+                if !eachItem.calculate2AnotherItemHaveSameTime(anotherItem: eachFatherItem) { continue }
+                eachItem.fatherItems.append(eachFatherItem)
+            }
+        }
+    }
+
+    /// 遍历每一个事件，处理此事件的父节点的层级
+    private func progressEachItemFathersLvl() {
+
+    }
+
+}
+
+/// 生成链表item的数据源
+class OutLinkItemModel: NSObject {
+
+    /// 事件的开始时间
+    var startTime: Date!
+
+    /// 事件的结束时间
+    var endTime: Date!
+}
+
+/// 链表item
+class LinkItem: NSObject {
+
+    /// 事件的开始时间
+    var startTime: Date!
+
+    /// 事件的结束时间
+    var endTime: Date!
+
+    /// 当前item所在的层级
+    var lvl: Int = 0
+
+    /// 当前item链条中层级层数
+    var lvlCount: Int = 0
+
+    /// 当前节点的父节点数组
+    var fatherItems: [LinkItem] = []
+
+    /// 为debug做的nickname
+    // var debugNickName: String = ""
+
+    /// 只处理开始时间和结束时间
+    init(model: OutLinkItemModel) {
+        self.startTime = model.startTime
+        self.endTime = model.endTime
+    }
+
+    /// 与另外一个事件比较，是否时间上有交集
+    func calculate2AnotherItemHaveSameTime(anotherItem: LinkItem) -> Bool {
+        if self.endTime <= anotherItem.startTime || self.startTime >= anotherItem.endTime {
+            return false
+        }
+        return true
+    }
+
+//    func progressFathersItemLvl(currentLv: Int?) {
+//        if currentLv == nil {
+//
+//        } else {
+//            // 最后一级
+//        }
+//        for eachItem in self.fatherItems {
+//            eachItem.lvlCount = self.lvl + 1
+//        }
+//    }
+}
+
+/// 链表item extension : comparable
+extension LinkItem: Comparable {
+    static func < (lhs: LinkItem, rhs: LinkItem) -> Bool {
+        if lhs.startTime == rhs.startTime {
+            if lhs.endTime == rhs.endTime {
+                return true
+            } else {
+                return lhs.endTime < rhs.endTime
+            }
+        } else {
+            return lhs.startTime < rhs.startTime
+        }
+    }
+}
